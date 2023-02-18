27902b97961c55136c8115b9bc0c6becc3850569
Alexander Ulmer <alexander.ulmer@gurdinet.at>
2023-02-18 16:22:37 +0100

    use vertex normal for simple shading

--- shader/v_animatedCharacter.glsl
+++ shader/v_animatedCharacter.glsl
@@ -4,7 +4,10 @@ layout (location = 0) in vec3 a_Position;
 layout (location = 1) in vec3 a_Normals;
 uniform mat4 u_MVP;
 
+out vec3 frag_normals;
+
 void main()
 {
+    frag_normals = a_Normals;
     gl_Position = u_MVP * vec4(a_Position, 1.0);
 }
